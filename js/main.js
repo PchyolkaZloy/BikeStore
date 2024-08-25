d321f051429576e019c84fc2c1324e586ca874d9
pchz <pchz@gmail.com>
2024-08-26 00:54:03 +0300

    Feat: Added js code that adds page load time tracking. Also in this script the navigation buttons are colored when the mouse is hovering and when the user is on the same page

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(() => {
+    const loadTimeCode = document.querySelector('.load-time');
+
+    window.addEventListener('load', () => {
+        loadTimeCode.innerHTML +=
+            `Page loaded in ${Math.floor(performance.mark('pageEnd').startTime * 100) / 100} ms.`;
+    });
+})();
+
+
+(() => {
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuItems = document.querySelectorAll('.nav__link');
+        const currentPage = document.location.pathname;
+
+        menuItems.forEach(item => {
+            if (currentPage.includes(item.getAttribute('href'))) {
+                item.classList.add('nav__link-active');
+            }
+        });
+
+        menuItems.forEach(item => {
+            item.addEventListener('mouseover', function () {
+                item.style.color = '#9c9c9c';
+            });
+            item.addEventListener('mouseout', function () {
+                item.style.color = '';
+            });
+        });
+    });
+})();
